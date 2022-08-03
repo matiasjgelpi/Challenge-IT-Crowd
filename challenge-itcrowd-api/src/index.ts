10e81f9ae94f8780fb172d4c4f8a0e3c9ef054f5
matiasjgelpi <matiasjgelpi@gmail.com>
2022-08-02 21:28:26 -0300

    first api commit
    configurin API development enviroment

---
+++ challenge-itcrowd-api/src/index.ts
@@ -0,0 +1,24 @@
+import 'dotenv/config'
+import express from 'express'
+// import { connectDb } from './database/dbConnection'
+// import blogPostRoutes from './routes/BlogPostRoutes'
+
+const cors = require('cors')
+
+// connectDb()
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+// app.use('/', blogPostRoutes)
+
+app.use('/', (_, res) => {
+  return res.status(404).send({ msg: 'resource not found' })
+})
+
+const PORT = ((process.env.PORT !== undefined) && process.env.PORT) || 3002
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`)
+})
